8d9ea0f001dae125753d5e3771d33e7c5285f59d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-21 05:05:46 -0400

    use glycin-ng

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,19 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm ristretto doxygen
+pacman -Syu --noconfirm ristretto doxygen rust
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano x265-mini ! llvm ! gdk-pixbuf ! librsvg
 
-# If the application needs to be manually built that has to be done down here
+pacman -Rdd --noconfirm glycin
+git clone https://github.com/QaidVoid/glycin-ng
+cd ./glycin-ng
+cargo build --release
+cargo build --release -p glycin-ng-libglycin-shim
+cp -v ./target/release/libglycin_ng.so /usr/lib
+cp -v ./target/release/libglycin_2.so  /usr/lib/libglycin-2.so.0
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
