e7adde4278fcaf25b7a17913117b2628a97cf229
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-21 05:34:16 -0400

    shim only

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,9 +15,7 @@ get-debloated-pkgs --add-common --prefer-nano x265-mini ! llvm ! gdk-pixbuf ! li
 pacman -Rdd --noconfirm glycin
 git clone https://github.com/QaidVoid/glycin-ng
 cd ./glycin-ng
-cargo build --release
 cargo build --release -p glycin-ng-libglycin-shim
-cp -v ./target/release/libglycin_ng.so /usr/lib
 cp -v ./target/release/libglycin_2.so  /usr/lib/libglycin-2.so.0
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
